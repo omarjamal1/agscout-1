a564268e15ee8dafef6eebcdb73edc7da5401647
=AhmadZarif <=azarif@milestonetechs.com>
2023-07-13 16:07:45 +0430

    update ruby version

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,28 +1,30 @@
 #!/bin/sh
+brew install ruby@2.5
+echo 'export PATH="/opt/homebrew/opt/ruby/bin:$PATH"' >> ~/.zshrc
+
 ruby --version
 # # Install CocoaPods using Homebrew.
-# brew install cocoapods
+brew install cocoapods
 
 # # Install Flutter
-# curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_2.0.6-stable.zip
-# unzip flutter_macos_2.0.6-stable.zip
+curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_2.0.6-stable.zip
+unzip flutter_macos_2.0.6-stable.zip
 
-# ls
 
-# export PATH="$PATH:`pwd`/flutter/bin"
+export PATH="$PATH:`pwd`/flutter/bin"
 
 # # Run Flutter doctor
-# flutter doctor
+flutter doctor
 
 
 # # Enable macos
-# flutter config --enable-macos-desktop
+flutter config --enable-macos-desktop
 
 # # Get packages
-# flutter packages get
+flutter packages get
 
 # # Update generated files
 # # flutter pub run build_runner build
 
 # # Build ios app
-# flutter build ios --no-codesign
+flutter build ios --no-codesign
