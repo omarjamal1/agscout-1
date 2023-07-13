58a57461f4c838d92d8f4d240591792a2401e1bf
=AhmadZarif <=azarif@milestonetechs.com>
2023-07-13 15:40:52 +0430

    check ruby version

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
+ruby --version
+# # Install CocoaPods using Homebrew.
+# brew install cocoapods
 
-# Install CocoaPods using Homebrew.
-brew install cocoapods
+# # Install Flutter
+# curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_2.0.6-stable.zip
+# unzip flutter_macos_2.0.6-stable.zip
 
-# Install Flutter
-curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_2.0.6-stable.zip
-unzip flutter_macos_2.0.6-stable.zip
+# ls
 
-ls
+# export PATH="$PATH:`pwd`/flutter/bin"
 
-export PATH="$PATH:`pwd`/flutter/bin"
-
-# Run Flutter doctor
-flutter doctor
+# # Run Flutter doctor
+# flutter doctor
 
 
-# Enable macos
-flutter config --enable-macos-desktop
+# # Enable macos
+# flutter config --enable-macos-desktop
 
-# Get packages
-flutter packages get
+# # Get packages
+# flutter packages get
 
-# Update generated files
-# flutter pub run build_runner build
+# # Update generated files
+# # flutter pub run build_runner build
 
-# Build ios app
-flutter build ios --no-codesign
+# # Build ios app
+# flutter build ios --no-codesign
